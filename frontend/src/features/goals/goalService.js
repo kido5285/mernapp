b31babfa24856c52b1652706b3d8baf575b8344f
kido5285 <kidosol5285@gmail.com>
2022-03-21 19:00:38 +0800

    fixed maybe??

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = `http://localhost:${process.env.PORT || 5000}/api/goals/`
+const API_URL = `/api/goals/`
 
 // Create new goal
 const createGoal = async (goalData, token) => {
